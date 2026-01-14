839c1f1367063cd4417dca5256fa0e63ac48196f
Tongyu-Yan <96633688+Tongyu-Yan@users.noreply.github.com>
2026-01-14 17:42:47 +0800

    fix load memory bugs

--- src/agent/service.py
+++ src/agent/service.py
@@ -280,7 +280,7 @@ class Agent:
         """Load the current memory from a file."""
         if not self.save_temp_file_path:
             return
-        file_name = os.path.join(self.save_temp_file_path, ".jsonl")
+        file_name = os.path.join(self.save_temp_file_path, "memory.jsonl")
         if os.path.exists(file_name):
             with open(file_name, "r", encoding=self.save_brain_conversation_path_encoding) as f:
                 lines = f.readlines()
